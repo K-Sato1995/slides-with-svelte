4c83b9d8c6814894f34b42c82ecda5dd51c98554
Sato Katsuki <satokatsuki@Satos-MacBook-Pro.local>
2022-05-26 13:06:12 +0900

    render slide ontent

--- src/routes/slides/[slug].svelte
+++ src/routes/slides/[slug].svelte
@@ -1,36 +1,49 @@
-<script>
-	import Markdoc from '@markdoc/markdoc';
+<script lang='ts'>
+	import Markdoc, { Tag } from '@markdoc/markdoc';
 	import { onMount, afterUpdate } from 'svelte';
 
+    interface SlidePage {
+        [id: number]: Tag[]
+    }
+
 	const doc = `
-    # Heading1 this is title
-    
-    Markdoc is a Markdown-based syntax and toolchain for creating custom documentation sites. Stripe created Markdoc to power [our public docs](http://stripe.com/docs).
-    
-    {% callout type="check" %}
-    Markdoc is open-source—check out its [source](http://github.com/markdoc/markdoc) to see how it works.
-    {% /callout %}
-    
-    # How is Markdoc different?
-    
-    Markdoc uses a fully declarative approach to composition and flow control, where other solutions… [Read more](/docs/overview).
-    
-    # Next steps
-    - [Install Markdoc](/docs/getting-started)
-    - [Explore the syntax](/docs/syntax)
+# Heading1 this is title
+
+Markdoc is a Markdown-based syntax and toolchain for creating custom documentation sites. Stripe created Markdoc to power [our public docs](http://stripe.com/docs).
+
+\`\`\`ts
+content.children.map((child) => {
+    if (child.name === 'h1') {
+        nodeCounter += 1;
+        pages[nodeCounter] = [child];
+    } else {
+        pages[nodeCounter].push(child);
+    }
+});
+\`\`\`
+
+
+{% callout type="check" %}
+Markdoc is open-source—check out its [source](http://github.com/markdoc/markdoc) to see how it works.
+{% /callout %}
+
+# How is Markdoc different?
+
+Markdoc uses a fully declarative approach to composition and flow control, where other solutions… [Read more](/docs/overview).
+
+
+# Next steps
+- [Install Markdoc](/docs/getting-started)
+- [Explore the syntax](/docs/syntax)
     `;
 
 	const ast = Markdoc.parse(doc);
-	const content = Markdoc.transform(ast);
+	const content = Markdoc.transform(ast) as Tag;
 
-	let pages = {};
+	let pages: SlidePage = { 0: [] };
 	let nodeCounter = 0;
 
-	// if(content.children[0].name !== 'h1') {
-	// throw new Error('You need a title: the first element must be h1.')
-	//  }
-
-	content.children.map((child) => {
+    content.children.map((child) => {
 		if (child.name === 'h1') {
 			nodeCounter += 1;
 			pages[nodeCounter] = [child];
